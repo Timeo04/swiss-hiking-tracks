448bb918d1c9ee2b896a87cf3707f5ba867ca1fc
wachtel17 <suisseglacier@gmail.com>
2024-02-15 14:52:56 +0100

    Titel hinzufügen

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -61,6 +61,10 @@
             </svg>
         </div>
 
+        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white text-center">
+            swiss-hiking-tracks
+        </h2>
+
         <div class="mt-16">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                 <a
