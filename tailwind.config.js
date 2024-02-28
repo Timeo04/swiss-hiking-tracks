2a2589518c3750192e7fdd1f96674ccc4ca7fe86
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-02-28 16:50:41 +0100

    adapt tailwindcss config for flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,29 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.svelte',
-        './resources/js/**/*.js',
+        './resources/js/**/*.{html,svelte,js,ts}',
+        './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
     ],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                // flowbite-svelte
+                primary: {
+                    50: '#FFF5F2',
+                    100: '#FFF1EE',
+                    200: '#FFE4DE',
+                    300: '#FFD5CC',
+                    400: '#FFBCAD',
+                    500: '#FE795D',
+                    600: '#EF562F',
+                    700: '#EB4F27',
+                    800: '#CC4522',
+                    900: '#A5371B'
+                }
             },
         },
     },
