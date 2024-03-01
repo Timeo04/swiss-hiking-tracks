b5fab36928904e891836609abd34140226e4b5f5
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-01 13:52:30 +0100

    implement tracks-list

--- resources/js/Pages/Tracks/Index.svelte
+++ resources/js/Pages/Tracks/Index.svelte
@@ -3,6 +3,8 @@
     import { router } from "@inertiajs/svelte";
     import { Button } from "flowbite-svelte";
     export let auth;
+    export let tracks;
+    console.log(tracks);
 </script>
 
 <svelte:head>
@@ -10,13 +12,40 @@
 </svelte:head>
 
 <AuthenticatedLayout {auth}>
-    <div slot="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Routen
-        </h2>
-    </div>
     <!-- <h1 class="text-2xl font-semibold">Routen</h1> -->
     <!-- <Button color="primary">Route erstellen</Button> -->
+    <h1 class="py-10 text-2xl font-semibold text-center">Wanderungen</h1>
+    {#if tracks == null || tracks.length === 0}
+        <p>Keine Routen vorhanden</p>
+    {:else}
+        <div class="flex flex-col gap-2 p-2">
+            {#each tracks as track}
+                <div
+                    class="flex items-center justify-between p-4 bg-white rounded-lg shadow-md"
+                >
+                    <div class="flex items-center">
+                        <div class="ml-4">
+                            <h2 class="text-xl font-semibold">{track.title}</h2>
+                            <p class="text-gray-500">
+                                {track.starting_location != null
+                                    ? track.starting_location + " - "
+                                    : ""}{track.destination_location != null
+                                    ? track.destination_location
+                                    : ""}
+                            </p>
+                        </div>
+                    </div>
+                    <button
+                        on:click={() =>
+                            router.visit(route("tracks.show", { track }))}
+                        class="px-4 py-2 text-white bg-primary-600 rounded-lg"
+                    >
+                        Öffnen
+                    </button>
+                </div>
+            {/each}
+        </div>
+    {/if}
     <button
         on:click={() => router.visit(route("tracks.create"))}
         class="group fixed bottom-20 right-4 p-0 w-12 h-12 border-primary-600 border-[3px] rounded-full hover:bg-primary-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
