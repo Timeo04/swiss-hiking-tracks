23574ad5ad49ff1a0b3f94daa5d63fed8312922f
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-01 22:17:33 +0100

    add Avatar Component

---
+++ resources/js/Components/Avatar.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Avatar } from "flowbite-svelte";
+    export let name = "";
+</script>
+
+<Avatar>
+    {#if name.split(" ").length > 1}
+        {name[0] + name.split(" ")[1][0]}
+    {:else if name != ""}
+        {name[0] + name[1]}
+    {/if}
+</Avatar>
