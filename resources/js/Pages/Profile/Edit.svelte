54b4efe5327c24b82692133b533b49038ba28b4e
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-01 22:17:51 +0100

    move things to settings-page

--- resources/js/Pages/Profile/Edit.svelte
+++ resources/js/Pages/Profile/Edit.svelte
@@ -20,34 +20,13 @@
 </svelte:head>
 
 <AuthenticatedLayout {auth}>
-    <h2 slot="header" class="font-semibold text-xl text-gray-800 leading-tight">
+    <!-- <h2 slot="header" class="font-semibold text-xl text-gray-800 leading-tight">
         Profile
-    </h2>
+    </h2> -->
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div
-                class="p-4 sm:p-8 bg-white shadow sm:rounded-lg flex gap-2 justify-start items-center"
-            >
-                <div class="shrink-0">
-                    <Avatar
-                        >{auth.user.name[0] +
-                            auth.user.name.split(" ")[1][0]}</Avatar
-                    >
-                </div>
-                <div class="grow truncate">
-                    <p class="truncate"><strong>{auth.user.name}</strong></p>
-                    <p class="truncate">{auth.user.email}</p>
-                </div>
-                <div>
-                    <Button
-                        color="primary"
-                        on:click={() => router.post(route("logout"))}
-                    >
-                        <ArrowRightToBracketOutline />
-                    </Button>
-                </div>
-            </div>
+           
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <UpdateProfileInformationForm
                     {mustVerifyEmail}
