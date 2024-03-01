b846e23ea4424262e1dfba9b3f130e51b8272550
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-01 22:38:42 +0100

    add back button

--- resources/js/Pages/Profile/Edit.svelte
+++ resources/js/Pages/Profile/Edit.svelte
@@ -23,10 +23,19 @@
     <!-- <h2 slot="header" class="font-semibold text-xl text-gray-800 leading-tight">
         Profile
     </h2> -->
-
+    <h1 class="py-10 text-2xl font-semibold text-center">Profil bearbeiten</h1>
+    <div>
+        <Button
+            outline
+            color="primary"
+            class="w-full"
+            on:click={() => router.visit(route("settings"))}
+        >
+            zurück
+        </Button>
+    </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-           
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <UpdateProfileInformationForm
                     {mustVerifyEmail}
