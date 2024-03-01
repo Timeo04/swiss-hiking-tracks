a75cb73e32d121d1e0576e1ed5071e6767823a2e
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-01 11:28:42 +0100

    set fillable attributes

--- app/Models/Track.php
+++ app/Models/Track.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Track extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'starting_location',
+        'destination_location',
+        'gpx_file',
+        'public'
+    ];
 }
