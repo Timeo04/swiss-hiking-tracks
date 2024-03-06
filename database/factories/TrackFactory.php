dab26ea42f58524f32ef925404de3376e2b91427
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-06 09:42:50 +0100

    implement TrackFactory

---
+++ database/factories/TrackFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Track>
+ */
+class TrackFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'starting_location' => fake()->city(),
+            'destination_location' => fake()->city(),
+            'gpx_file' => 'test.gpx',
+        ];
+    }
+}
