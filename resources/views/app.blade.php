0b7afeb59789ab51c570abd6ab383821656934fc
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-07 15:16:45 +0100

    add comments, change default title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Titel -->
+        <title inertia>{{ config('app.name', 'SwissHikingTracks') }}</title>
 
-        <!-- Fonts -->
+        <!-- Fonts laden -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=kanit:400,500,600" rel="stylesheet" />
 
