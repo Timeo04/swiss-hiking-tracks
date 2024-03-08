6b452e2fb743e298b639cbb7e9e3bad31775652e
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-08 14:57:31 +0100

    cleanup

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.svelte
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.svelte
@@ -1,8 +1,6 @@
 <script>
     import InputError from "@/Components/InputError.svelte";
-    import InputLabel from "@/Components/InputLabel.svelte";
     import PrimaryButton from "@/Components/PrimaryButton.svelte";
-    import TextInput from "@/Components/TextInput.svelte";
     import { useForm } from "@inertiajs/svelte";
     import { FloatingLabelInput } from "flowbite-svelte";
     import { sineInOut } from "svelte/easing";
