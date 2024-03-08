c8fb778c2701ffd3310895f588c9e28d017eb9b6
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-08 14:14:02 +0100

    cleanup

--- resources/js/Components/Dropdown.svelte
+++ resources/js/Components/Dropdown.svelte
@@ -7,20 +7,6 @@
     export let width = "48";
     export let contentClasses = "py-1 bg-white";
     export let open = false;
-    // const props = defineProps({
-    //     align: {
-    //         type: String,
-    //         default: 'right',
-    //     },
-    //     width: {
-    //         type: String,
-    //         default: '48',
-    //     },
-    //     contentClasses: {
-    //         type: String,
-    //         default: 'py-1 bg-white',
-    //     },
-    // });
 
     const closeOnEscape = (e) => {
         if (open && e.key === "Escape") {
@@ -43,8 +29,6 @@
     } else {
         alignmentClasses = "origin-top";
     }
-
-    // const open = ref(false);
 </script>
 
 <div class="relative">
@@ -71,14 +55,6 @@
             on:click={() => (open = false)}
         ></div>
 
-        <!-- <Transition
-            enter-active-class="transition ease-out duration-200"
-            enter-from-class="opacity-0 scale-95"
-            enter-to-class="opacity-100 scale-100"
-            leave-active-class="transition ease-in duration-75"
-            leave-from-class="opacity-100 scale-100"
-            leave-to-class="opacity-0 scale-95"
-        > -->
         <div
             role="menu"
             tabindex="0"
@@ -98,6 +74,5 @@
                 <slot name="content" />
             </div>
         </div>
-        <!-- </Transition> -->
     {/if}
 </div>
