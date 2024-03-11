b5800a53717604429e1e7dfef990c6b524c8ede1
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-11 08:57:25 +0100

    cleanup

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -23,7 +23,6 @@
         {#if auth.user != null}
             <a
                 use:inertia
-                v-if="$page.props.auth.user"
                 href={route("dashboard")}
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                 >Dashboard</a
