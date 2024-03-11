de0fa513672c12fda80ca511a3b2fc2f07daa9fb
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-11 17:58:44 +0100

    store geojson in database

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -41,8 +41,9 @@ class TrackController extends Controller
         // Validate the request data
         $request->validate([
             'title' => ['required', 'max:255'],
-            'starting_location' => ['string', 'max:255'],
-            'destination_location' => ['string', 'max:255'],
+            'starting_location' => ['nullable', 'string', 'max:255'],
+            'destination_location' => ['nullable', 'string', 'max:255'],
+            'geojson' => ['required', 'json'],
             // 'gpx_file' => ['required', 'file'],
         ]);
 
@@ -51,7 +52,8 @@ class TrackController extends Controller
             'title' => $request->input('title'),
             'starting_location' => $request->input('starting_location'),
             'destination_location' => $request->input('destination_location'),
-            'gpx_file' => "test.gpx",
+            // 'gpx_file' => "test.gpx",
+            'geojson' => $request->input('geojson'),
             'user_id' => auth()->id(),
         ]);
 
