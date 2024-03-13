20b15afc4ee580ebc6b0bf923f6010731c3f6e3b
wachtel17 <suisseglacier@gmail.com>
2024-03-13 15:36:38 +0100

    new Tag Model

---
+++ app/Models/Tag.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    /**
+     * Return the tracks which belong to a tag
+     *
+     * @return BelongsToMany
+     */
+    public function tracks(): BelongsToMany
+    {
+        return $this->belongsToMany(Track::class);
+    }
+}
