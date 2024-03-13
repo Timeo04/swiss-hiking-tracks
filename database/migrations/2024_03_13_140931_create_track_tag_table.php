1894e9c44c21e096bce83851eb29a91a4a5ec98a
wachtel17 <suisseglacier@gmail.com>
2024-03-13 15:24:11 +0100

    create database migrations

---
+++ database/migrations/2024_03_13_140931_create_track_tag_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('track_tag', function (Blueprint $table) {
+            $table->unsignedBigInteger('track_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('track_id')->references('id')->on('tracks');
+            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->primary(['tag_id', 'track_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('track_tag');
+    }
+};
