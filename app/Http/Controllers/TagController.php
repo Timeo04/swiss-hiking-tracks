979a75b2f08d637c69c8a8ad7d9a5122e45c26a3
wachtel17 <suisseglacier@gmail.com>
2024-03-15 21:01:51 +0100

    edit TagController

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -4,15 +4,66 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-// Tag-Model importieren
 use App\Models\Tag;
-use Inertia\Inertia;
 use DOMDocument;
+// Inertia importieren
+use Inertia\Inertia;
 
 class TagController extends Controller
 {
-   
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
 
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tag $tag)
+    {
+        echo 'Hallo';
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
