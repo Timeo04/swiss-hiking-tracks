f3cd1b3741e48795be777e1b7ce42ae45a28ff95
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-21 17:29:58 +0100

    add slides

--- resources/js/Components/Tracks/InformationsSwiper.svelte
+++ resources/js/Components/Tracks/InformationsSwiper.svelte
@@ -10,7 +10,10 @@
     <swiper-slide class="h-[500px] relative rounded-2xl overflow-clip">
         <Map {track} />
     </swiper-slide>
-    <swiper-slide class="h-[500px] relative rounded-2xl overflow-clip">
-        <h4>Wetter</h4>
+    <swiper-slide class="h-[500px] bg-gray-300 relative rounded-2xl overflow-clip">
+        <h3 class="text-2xl">Wetter</h3>
+    </swiper-slide>
+    <swiper-slide class="h-[500px] bg-gray-300 relative rounded-2xl overflow-clip">
+        <h3 class="text-2xl">Sicherheit und Notfallnummern</h3>
     </swiper-slide>
 </Swiper>
