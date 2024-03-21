96081b92d479457ace4c85e76dd1f29aa2ea88c0
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-03-21 17:06:50 +0100

    add InformationsSwiper Component

---
+++ resources/js/Components/Tracks/InformationsSwiper.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Swiper from "@/Components/Swiper.svelte";
+    import Map from "./Map.svelte";
+
+    export let track;
+    let swiper = null;
+</script>
+
+<Swiper bind:this={swiper}>
+    <swiper-slide class="h-[500px] relative rounded-2xl overflow-clip">
+        <Map {track} />
+    </swiper-slide>
+    <swiper-slide class="h-[500px] relative rounded-2xl overflow-clip">
+        <h4>Wetter</h4>
+    </swiper-slide>
+</Swiper>
