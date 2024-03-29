736af3eeb3346e01e873d595ee10c155faba46ba
g0nDra <yannisbontempi@gmail.com>
2024-03-29 18:53:50 +0100

    removed comments

--- resources/js/Components/WeatherChart.svelte
+++ resources/js/Components/WeatherChart.svelte
@@ -1,7 +1,5 @@
 <script>
     import { Chart, Line, Bar } from "svelte-chartjs";
-    // Funktion zum Berechnen der Länge importieren
-    // import { calculateHeightPoints } from "@/utils/geojson/linestring";
     import {
         Chart as ChartJS,
         Title,
@@ -14,11 +12,10 @@
         CategoryScale,
         Filler,
     } from "chart.js";
-    import { AngleRightOutline } from "flowbite-svelte-icons";
-    // import colors from "tailwindcss/colors";
 
     export let foreCastData;
     export let currentTime;
+    
     let time = [];
     const timePattern = /\d{2}:\d{2}/;
 
@@ -34,12 +31,6 @@
     let foreCastPrecipitation = [];
     foreCastPrecipitation = foreCastData.minutely_15.precipitation;
 
-    // console.log("ForeCast", foreCastTemp);
-    // console.log("Current Time", currentTime);
-    // console.log("Time", time);
-    // console.log("ForeCastOne", foreCastData);
-    // console.log("ForeCastPrecipitation", foreCastPrecipitation);
-
     ChartJS.register(
         Title,
         Tooltip,
@@ -52,96 +43,25 @@
         CategoryScale,
     );
 
-    //     const mixedChart = new Chart(ctx, {
-    //    type: 'bar',
-    //    data: {
-    //        datasets: [{
-    //            label: 'Bar Dataset',
-    //            data: [10, 20, 30, 40],
-    //            // this dataset is drawn below
-    //            order: 2
-    //        }, {
-    //            label: 'Line Dataset',
-    //            data: [10, 10, 10, 10],
-    //            type: 'line',
-    //            // this dataset is drawn on top
-    //            order: 1
-    //        }],
-    //        labels: ['January', 'February', 'March', 'April']
-    //    },
-    //    options: options
-    // });
-
-    // let forecastDataPrecipitation = {
-    //     labels: time,
-    //     datasets: [
-    //         {
-    //             label: "Niederschlag (mm)",
-    //             // data: foreCastTemp.map((temp, index) => ({
-    //             // y: temp,
-    //             // x: (index * 15)/60,
-    //             // x: foreCastData.minutely_15.time,
-    //             // })),
-    //             data: foreCastPrecipitation,
-    //             // fill: "start",
-    //             // backgroundColor: "#EF562F",
-    //             borderColor: "#EFEFEF",
-    //         },
-    //     ],
-    // };
-    // let forecastDataTemp = {
-    //     labels: time,
-    //     datasets: [
-    //         {
-    //             label: "Temperatur (C°)",
-    //             // data: foreCastTemp.map((temp, index) => ({
-    //             // y: temp,
-    //             // x: (index * 15)/60,
-    //             // x: foreCastData.minutely_15.time,
-    //             // })),
-    //             data: foreCastTemp,
-    //             // fill: "start",
-    //             // backgroundColor: "#EF562F",
-    //             borderColor: "#EF562F",
-    //         },
-    //     ],
-    // };
     let forecastData = {
         labels: time,
-        // data: {
         datasets: [
             {
                 type: "line",
                 label: "Temperatur (C°)",
-                // data: foreCastTemp.map((temp, index) => ({
-                // y: temp,
-                // x: (index * 15)/60,
-                // x: foreCastData.minutely_15.time,
-                // })),
                 data: foreCastTemp,
-                // fill: "start",
-                // backgroundColor: "#EF562F",
-                borderColor: "#EF562F",
-                // order: 1,
+                borderColor: "#EF562F",                
                 yAxisID: "y1",
             },
             {
                 type: "bar",
                 label: "Niederschlag (mm)",
-                // data: foreCastTemp.map((temp, index) => ({
-                // y: temp,
-                // x: (index * 15)/60,
-                // x: foreCastData.minutely_15.time,
-                // })),
                 data: foreCastPrecipitation,
-                // fill: "start",
                 backgroundColor: "#0000FF",
-                // borderColor: "#FF0000",
                 order: 2,
                 yAxisID: "y2",
             },
         ],
-        // },
     };
     const forecastOptions = {
         plugins: {
@@ -165,17 +85,9 @@
                         size: 10,
                     },
                 },
-                // type: "linear",
                 ticks: {
                     callback: (value) => time[value],
-                },
-                // ticks: {
-                //     callback: (value) => `${Math.round(value * 10) / 10} km`,
-                // },
-                // max: heightPoints[heightPoints.length - 1].distance / 1000,
-                // min: 0,
-                // max: 24,
-                // beginAtZero: true,
+                },  
             },
             y1: {
                 title: {
@@ -187,10 +99,6 @@
                     },
                 },
                 type: "linear",
-                // suggestedMax: Math.max(
-                //     minHeight + 150,
-                //     maxHeight + (maxHeight - minHeight) * 0.15,
-                // ),
             },
             y2: {
                 title: {
@@ -218,5 +126,4 @@
 
 <div class="w-full h-full max-h-[400px] flex justify-center items-center">
     <Bar data={forecastData} options={forecastOptions} />
-    <!-- <Line data={forecastDataPrecipitation} options={forecastOptions} /> -->
 </div>
