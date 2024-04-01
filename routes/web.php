f3a3822ac69be6f584fb9702b2b55fa55c4725ae
Cedric Bolleter <cedric.bolleter@gmail.com>
2024-04-01 22:53:16 +0200

    Add comments, cleanup

--- routes/web.php
+++ routes/web.php
@@ -46,14 +46,19 @@ Route::get('/', function () {
 
 // Dashboard zurückgeben, wenn der User eingeloggt ist
 Route::get('/dashboard', function () {
+    // Tracks des eingeloggten Users laden
     $tracks = auth()->user()->tracks;
+    // Alle Tags der Tracks abrufen
+    $tracks->load('tags');
     if ($tracks != []) {
         foreach ($tracks as &$track) {
-            // $track = [...$track, 'image' => $track->getFirstMedia('images')];
+            // Erstes Bild des Tracks abrufen
             $image = $track->getFirstMedia('images');
+            // Bild-URL dem Track hinzufügen
             $track['image_url'] = $image != null ? $image->getUrl() : null;
         }
     }
+    // Dashboard-View mit Übergabewert tracks zurückgeben
     return Inertia::render('Dashboard', [
         'tracks' => $tracks,
     ]);
@@ -61,32 +66,46 @@ Route::get('/dashboard', function () {
 
 // Settings-Route-Gruppe, die nur eingeloggten Usern zur Verfügung steht
 Route::prefix('/settings')->middleware('auth')->group(function () {
+    // Profil-Bearbeiten-View zurückgeben
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Wandergeschwindigkeit setzen
     Route::post('/profile/hiking-speed', [ProfileController::class, 'setHikingSpeed'])->name('profile.setHikingSpeed');
+    // Settings-View zurückgeben
     Route::get('/', [ProfileController::class, 'settings'])->name('settings')->middleware('auth');
 });
 
-// Tracks-Resource-Route-Gruppe, die nur eingeloggten Usern zur Verfügung steht
+// Tracks-Resource-Route, die nur eingeloggten Usern zur Verfügung steht
 Route::resource('tracks', TrackController::class)->middleware('auth')->where(['track' => '[0-9]+']);
-Route::get('tracks/{track}/gpx', [TrackController::class, 'gpx'])->name('tracks.gpx')->middleware('auth');
-Route::get('tracks/{track}/swisstopo', [TrackController::class, 'swisstopo'])->name('tracks.swisstopo')->middleware('auth');
-Route::post('tracks/{track}/image', [TrackController::class, 'storeImage'])->name('tracks.storeImage')->middleware('auth');
-Route::post('tracks/{track}/image/order', [TrackController::class, 'updateImageOrder'])->name('tracks.updateImageOrder')->middleware('auth');
-Route::delete('tracks/{track}/image/{image}', [TrackController::class, 'destroyImage'])->name('tracks.destroyImage')->middleware('auth');
-Route::post('tracks/{track}/share', [TrackController::class, 'share'])->name('tracks.share')->middleware('auth');
-Route::delete('tracks/{track}/share', [TrackController::class, 'unshare'])->name('tracks.unshare')->middleware('auth');
 
+Route::prefix('/tracks')->middleware('auth')->group(function () {
+    // GPX-Export-Route
+    Route::get('{track}/gpx', [TrackController::class, 'gpx'])->name('tracks.gpx')->middleware('auth');
+    // Weiterleitung zu SwissTopo-App mit Track
+    Route::get('{track}/swisstopo', [TrackController::class, 'swisstopo'])->name('tracks.swisstopo')->middleware('auth');
+    // Bild-Upload
+    Route::post('{track}/image', [TrackController::class, 'storeImage'])->name('tracks.storeImage')->middleware('auth');
+    // Bild-Reihenfolge ändern
+    Route::post('{track}/image/order', [TrackController::class, 'updateImageOrder'])->name('tracks.updateImageOrder')->middleware('auth');
+    // Bild löschen
+    Route::delete('{track}/image/{image}', [TrackController::class, 'destroyImage'])->name('tracks.destroyImage')->middleware('auth');
+    // Track teilen
+    Route::post('{track}/share', [TrackController::class, 'share'])->name('tracks.share')->middleware('auth');
+    // Track teilen aufheben
+    Route::delete('{track}/share', [TrackController::class, 'unshare'])->name('tracks.unshare')->middleware('auth');
+});
+
+// Geteilten Track anzeigen
 Route::get('tracks/{track_share_url}', [TrackController::class, 'showShare'])->name('tracks.showShare')->where(['track_share_url' => '[A-Za-z]+']);
 
 // Tags-Resource-Route-Gruppe, die nur eingeloggten Usern zur Verfügung steht
 Route::resource('tags', TagController::class)->middleware('auth');
 
-//Route-Gruppe Verbindung Tags mit Tracks, die nur eingeloggten Usern zur Verfügung steht
+// Verbindung Tags mit Tracks Route, die nur eingeloggten Usern zur Verfügung steht
 Route::post('tracks/{track}/tag', [TrackController::class, 'tag'])->name('tracks.tag')->middleware('auth');
 
-//Route-Gruppe Verbindung Tags mit Tracks aufheben, die nur eingeloggten Usern zur Verfügung steht
+// Verbindung Tags mit Tracks aufheben Route, die nur eingeloggten Usern zur Verfügung steht
 Route::post('tracks/{track}/untag', [TrackController::class, 'untag'])->name('tracks.untag')->middleware('auth');
 
 require __DIR__ . '/auth.php';
